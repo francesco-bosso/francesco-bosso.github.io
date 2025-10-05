63cf0e39c7f081e17e3113c7b1461dfbf4f76f8f
FBosso <francesco.bosso@mail.polimi.it>
2025-10-05 19:17:04 +0200

    change nuxt config

--- _nuxt/static/1759683995/artificial-intelligence/6/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1759683995",layout:"default",error:a,serverRendered:true,routePath:"\u002Fartificial-intelligence\u002F6",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a,buildAssetsDir:"_nuxt\u002F"}}}}(null));
