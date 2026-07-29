d283b9c18dd755bb5cebce1fcc005e98e9c871d8
FBosso <francesco.bosso@mail.polimi.it>
2026-07-29 23:33:15 +0200

    update index ui

--- _nuxt/static/1785360462/about-me/state.js
+++ _nuxt/static/1785360462/about-me/state.js
@@ -1,5 +1,1 @@
-<<<<<<<< HEAD:_nuxt/static/1785360462/about-me/state.js
 window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1785360462",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout-me",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
-========
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1785095291",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout-me",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
->>>>>>>> c4b681063d8689fb10aa6548a9b99ba9c5c0697e:_nuxt/static/1785095291/about-me/state.js
